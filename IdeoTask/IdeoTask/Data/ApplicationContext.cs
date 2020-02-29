b7400c2bbeb98974049f4efb5e8bfb892e683948
Bohdan Filimonyuk <bogdan1999kovel@gmail.com>
2020-02-29 11:46:31 +0100

    Project initial commit

---
+++ IdeoTask/IdeoTask/Data/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using IdeoTask.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdeoTask.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Catalog> Catalogs { get; set; }
+    }
+}
---
+++ IdeoTask/IdeoTask/Models/Catalog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdeoTask.Models
+{
+    public class Catalog
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [ForeignKey("Catalog")]
+        public virtual Catalog ParentCatalog { get; set; }
+    }
+}
